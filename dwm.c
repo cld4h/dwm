4033b4e649b411cd5bf6d98721e78f8a0ce5fde8
cld4h <20869428+cld4h@users.noreply.github.com>
2021-03-26 20:30:42 +0800

    Patched systrayonleft
    https://dwm.suckless.org/patches/systray/dwm-systray-20210217-61bb8b2.diff

--- dwm.c
+++ dwm.c
@@ -1621,7 +1621,7 @@ resize(Client *c, int x, int y, int w, int h, int interact)
 void
 resizebarwin(Monitor *m) {
 	unsigned int w = m->ww;
-	if (showsystray && m == systraytomon(m))
+	if (showsystray && m == systraytomon(m) && !systrayonleft)
 		w -= getsystraywidth();
 	XMoveResizeWindow(dpy, m->barwin, m->wx, m->by, w, bh);
 }
